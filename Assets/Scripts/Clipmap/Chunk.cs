fcab0e87d3f879801727312d7fa59e81bcf03183
Brian Hoy <brianhoy4@gmail.com>
2019-08-04 22:31:05 -0400

    Added clipmap/chunk classes

---
+++ Assets/Scripts/Clipmap/Chunk.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class Chunk {
+	public RT.CompactSVO svo;
+	public Vector3 offset;
+}
--- Assets/Scripts/Clipmap/Clipmap.cs
+++ Assets/Scripts/Clipmap/Clipmap.cs
@@ -1,5 +1,17 @@
 using UnityEngine;
 
 public class Clipmap : MonoBehaviour {
-	public c
+	public Chunk chunk1;
+	public Chunk chunk2;
+
+	void Start() {
+		chunk1 = new Chunk();
+		chunk2 = new Chunk();
+
+		chunk1.svo = new RT.CompactSVO(SampleFunctions.functions[1], 4);
+		chunk2.svo = new RT.CompactSVO(SampleFunctions.functions[1], 4);
+
+		chunk1.offset = Vector3.zero;
+		chunk2.offset = Vector3.one;
+	}
 }
